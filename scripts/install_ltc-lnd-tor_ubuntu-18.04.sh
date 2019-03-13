3ccac263b3db285983fecc547e70cac53bb250b2
teletan <47466290+teletan@users.noreply.github.com>
2019-03-13 04:04:37 +0000

    Update install_ltc-lnd-tor_ubuntu-18.04.sh

--- scripts/install_ltc-lnd-tor_ubuntu-18.04.sh
+++ scripts/install_ltc-lnd-tor_ubuntu-18.04.sh
@@ -90,7 +90,6 @@ sudo apt install -y golang-1.11
 echo 'export GOPATH=~/gocode' >> ~/.bash_profile
 echo 'export PATH=$PATH:$GOPATH/bin:/usr/lib/go-1.11/bin' >> ~/.bash_profile
 echo alias lncli=\'~/gocode/bin/lncli --network mainnet --chain litecoin\' >> ~/.bash_profile
-echo alias go=\'/usr/lib/go-1.11/bin/go\' >> ~/.bash_profile
 . .bash_profile
 
 # install ltcd
